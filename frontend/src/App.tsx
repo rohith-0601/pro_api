495fc66c567a5a9688125772a1a2ff739cba49e3
rohith-0601 <rohithperugu3@gmail.com>
2025-09-02 17:27:09 +0530

    internship work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -80,8 +80,7 @@ export default function App() {
           >
             Send
           </button>
-          <p>working as intern so will come back soon</p>
-          <p>intern meet today</p>
+          
         </div>
       </div>
     </div>
